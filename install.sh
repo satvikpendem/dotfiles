5ef511a387e4b0df4878a61373bf9f7e5bf5b6a6
Satvik Pendem <42670561+satvikpendem@users.noreply.github.com>
2022-08-08 17:01:08 -0400

    Add starship

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ apt_packages="batcat build-essential clang cmake fd-find llvm libc++-dev libstdc
 brew_packages="bat curl deno fd llvm neovim vim wget zld"
 brew_cask_packages="alt-tab appcleaner chrome-remote-desktop-host cloudflare-warp firefox flutter github google-chrome iterm2 linear-linear lunar macs-fan-control messenger moonlight mpv neovide nightfall nordvpn parsec qbittorrent rectangle slack stats visual-studio-code zoom"
 
-cargo_packages="cargo-audit cargo-cranky cargo-do cargo-edit cargo-tarpaulin cargo-watch fnm hyperfine git-delta skim tealdeer"
+cargo_packages="cargo-audit cargo-cranky cargo-do cargo-edit cargo-tarpaulin cargo-watch fnm hyperfine git-delta skim starship tealdeer"
 
 installer="UNKNOWN"
 
