d72745b6060eab44442e05072bfab9a5e136f99c
Satvik Pendem <satvik.pendem@gmail.com>
2023-05-18 20:55:11 -0400

    Add yt-dlp

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ common_packages="cmake curl git htop llvm unzip vim wget zsh"
 apt_packages="build-essential clang fd-find libc++-dev libstdc++-10-dev libssl-dev pkg-config zlib1g zlib1g-dev"
 
 brew_taps="michaeleisel/homebrew-zld epk/epk"
-brew_packages="deno fd llvm neovim zld"
+brew_packages="deno fd llvm neovim yt-dlp zld"
 brew_cask_packages="alt-tab appcleaner chrome-remote-desktop-host firefox font-sf-mono-nerd-font github google-chrome iterm2 linear-linear lunar maccy messenger neovide nordvpn parsec qbittorrent rectangle slack stats visual-studio-code"
 
 cargo_packages="bat cargo-audit cargo-cranky cargo-do cargo-edit cargo-expand cargo-nextest cargo-tarpaulin cargo-update cargo-watch erdtree exa fnm hyperfine git-delta ripgrep skim starship tealdeer xh zoxide"
